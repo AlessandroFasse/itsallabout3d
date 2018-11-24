74911c2c14be581439d6525728cf1c7a7c8193a2
Alessandro Fasse <alessandrofasse@Alessandros-MacBook-Pro.local>
2018-11-24 22:56:12 +0100

    updates to the exercises

--- exercise1.py
+++ exercise1.py
@@ -11,11 +11,11 @@ Write a down the vertices and faces of a cube as given in the picture
 
 
 vertices = np.array([[0,0,0],
-            [1,0,0],
-            [0,1,0],
-            [1,1,0]])
+                     [1,0,0],
+                     [0,1,0],
+                     [1,1,0]])
 
 faces = np.array([[0,1,3],
-         [3,2,0]])
+                  [3,2,0]])
 
 print_3d_mesh(vertices, faces)
