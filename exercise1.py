2cde42341b3276a37ce7074c54fef322b3263e6a
Alessandro Fasse <alessandrofasse@Alessandros-MacBook-Pro.local>
2018-11-24 16:28:02 +0100

    added print functions for easier access

--- exercise1.py
+++ exercise1.py
@@ -1,4 +1,6 @@
 import numpy
+from tools import *
+
 
 '''
 Exercise 1
@@ -8,12 +10,12 @@ Write a down the vertices and faces of a cube as given in the picture
 
 
 
-vertices = [[0,0,0],
+vertices = np.array([[0,0,0],
             [1,0,0],
             [0,1,0],
-            [1,1,0]]
+            [1,1,0]])
 
-faces = [[0,1,3],
-         [3,2,0]]
+faces = np.array([[0,1,3],
+         [3,2,0]])
 
-print(vertices)
+print_3d_mesh(vertices, faces)
--- tools.py
+++ tools.py
@@ -1,5 +1,29 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-def print_2d_mesh(vertices, faces):
-    plt.scatter(vertices[:, 0], vertices[:, 1])
+def print_2d_mesh(vertices, faces): # prints the x and y coordintes of the mesh including the edges    
+    for i in range(np.shape(faces)[0]):
+        for j in range(3):
+            x = np.array([[vertices[faces[i,j], 0]],
+                          [vertices[faces[i,(j+1)%3], 0]]])
+            y = np.array([[vertices[faces[i,j], 1]],
+                          [vertices[faces[i,(j+1)%3], 1]]])
+            plt.plot(x, y, c='#FF8B00', zorder=0)
+    plt.scatter(vertices[:, 0], vertices[:, 1], c='k', zorder=1, s=80)
+    plt.show()
+
+def print_3d_mesh(vertices, faces): # prints the mesh in 3d
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    for i in range(np.shape(faces)[0]):
+        for j in range(3):
+            x = np.array([[vertices[faces[i,j], 0]],
+                          [vertices[faces[i,(j+1)%3], 0]]])
+            y = np.array([[vertices[faces[i,j], 1]],
+                          [vertices[faces[i,(j+1)%3], 1]]])
+            z = np.array([[vertices[faces[i,j], 2]],
+                          [vertices[faces[i,(j+1)%3], 2]]])
+            ax.plot(x, y, c='#FF8B00', zorder=0)
+    ax.scatter(vertices[:,0], vertices[:,1], vertices[:, 2], c='k', s=80)
+    plt.show()
