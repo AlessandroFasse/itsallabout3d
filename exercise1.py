933622357ca9aa2788b737be5c1e46560e1bd60d
Alessandro Fasse <alessandrofasse@Alessandros-MacBook-Pro.local>
2018-11-25 19:05:18 +0100

    cleaned exercise

--- exercise1.py
+++ exercise1.py
@@ -3,8 +3,8 @@ from tools import *
 
 ## Exercise 1 - It's all about 3D Workshop
 
-vertices = np.array([[0,0,0], [1,0,0]], dtype=np.float16)
-faces = np.array([[0,1,1]], dtype=np.uint8)
+vertices = np.array([], dtype=np.float16)
+faces = np.array([], dtype=np.uint8)
 
 # Check your result with a 3d print of the vertices and edges
 print_3d_mesh(vertices, faces)
